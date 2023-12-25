31aa1bf9ce0785eed4a95710b787edbe75309572
Marco Aurélio Andrade Júnior <juninho.meritus@hotmail.com>
2023-12-25 22:49:45 +0000

    Adding scripts `lint:check and lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Esta é uma implementação do https://www.tabnews.com.br voltada para treinar programação para o curso https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
 function Home() {
-    return <h1>Clone do Tab-news agora está deployado pela <a href="https://vercel.com" target="newtab">Vercel</a></h1>
+  return (
+    <h1>
+      Clone do Tab-news agora está deployado pela{" "}
+      <a href="https://vercel.com" target="newtab">
+        Vercel
+      </a>
+    </h1>
+  );
+}
+
+function teste() {
+  console.log('identação errada');
 }
 
 export default Home;
