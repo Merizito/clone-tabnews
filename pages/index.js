2c4167cd8e800dade2c276c69863ddf75caea14b
Marco Aurélio Andrade Júnior <juninho.meritus@hotmail.com>
2024-05-03 03:06:55 +0000

    Adjusting 'in construction' animation

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 function Home() {
   return (
     <>
+    <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script>
       <div class="title">
         <h1>
           Clone do Tab-news está em construção e deployado pela{" "}
@@ -9,8 +10,6 @@ function Home() {
           </a>
         </h1>
       </div>
-
-      <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script>
 
       <dotlottie-player src="https://lottie.host/e4da4fb5-853b-4ca5-ace0-4503264f27da/uWdggEe6Vj.json" background="transparent" speed="1" style="width: 300px; height: 300px;" loop autoplay></dotlottie-player>
     </>
