6bd3c405765e22194a716f46379238510af0de9b
Marco Aurélio Andrade Júnior <juninho.meritus@hotmail.com>
2024-05-03 03:04:18 +0000

    Adjustments in animation

--- pages/index.js
+++ pages/index.js
@@ -10,9 +10,9 @@ function Home() {
         </h1>
       </div>
 
-      <div class="iframe_gif">
-        <iframe src="https://lottie.host/embed/e4da4fb5-853b-4ca5-ace0-4503264f27da/uWdggEe6Vj.json"></iframe>
-      </div>
+      <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script>
+
+      <dotlottie-player src="https://lottie.host/e4da4fb5-853b-4ca5-ace0-4503264f27da/uWdggEe6Vj.json" background="transparent" speed="1" style="width: 300px; height: 300px;" loop autoplay></dotlottie-player>
     </>
   );
 }
