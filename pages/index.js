eba1f0333471ab6f9c9314a1dc30c4a7a9cd91a0
Marco Aurélio Andrade Júnior <juninho.meritus@hotmail.com>
2024-05-03 03:02:04 +0000

    Added 'in construction' animation.

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
 function Home() {
   return (
-    <h1>
-      Clone do Tab-news agora está deployado pela{" "}
-      <a href="https://vercel.com" target="newtab">
-        Vercel
-      </a>
-    </h1>
+    <>
+      <div class="title">
+        <h1>
+          Clone do Tab-news está em construção e deployado pela{" "}
+          <a href="https://vercel.com" target="newtab">
+            Vercel
+          </a>
+        </h1>
+      </div>
+
+      <div class="iframe_gif">
+        <iframe src="https://lottie.host/embed/e4da4fb5-853b-4ca5-ace0-4503264f27da/uWdggEe6Vj.json"></iframe>
+      </div>
+    </>
   );
 }
 
